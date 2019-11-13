3b04b017ade9f38c1598ff6a298862875c221ee0
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-11-13 21:01:17 +0300

    fix: snapshot testing

---
+++ components/__test__/button.snapshot.test.tsx
@@ -0,0 +1,13 @@
+// components/__tests__/button.snapshot.test.tsx
+import * as React from "react";
+import * as TestRenderer from 'react-test-renderer';
+import Button from "../Button";
+
+
+it('renders correctly', () => {
+    const tree = TestRenderer.create(<Button buttonText="Some Text" />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
+
+
+//console.log(testRenderer.toJSON());
